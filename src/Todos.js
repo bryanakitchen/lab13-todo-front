01934d0b313fbfd37fd71d10a6796f42b69b7720
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-11-11 17:58:28 -0500

    basic fetch todos

--- src/Todos.js
+++ src/Todos.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import request from 'superagent';
 
 export default class Todos extends React.Component {
+    state = {
+        todos: [],
+        choreName: '',
+        loading: false,
+    }
     
-    
+    componentDidMount = async () => {
+        await this.fetchTodos()
+    }
+
+    fetchTodos = async () => {
+        const { token } = this.props;
+
+        await this.setState({ loading: true });
+        const response = await request
+            .get('https://lab12-todo.herokuapp.com/api/todos')
+            .set('Authorization', token);
+
+        await this.setState({ todos: response.body, loading: false })
+    }
+
     render() {
+        const { loading, todos } = this.state;
         return (
             <div>
-                <h3>This is your ToDo page</h3>
+                <h3>Here is your ToDo List</h3>
+                {
+                    loading 
+                    ? 'Loading... Please wait.'
+                    : todos.map(todo => <div> 
+                        Chore: {todo.chore}; Status: {todo.completed}
+                    </div>)
+                }
+
             </div>
         )
     }
